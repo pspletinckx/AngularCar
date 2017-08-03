317d1c440f8b01a4249d24aa47c02835c0faa4ea
Pieter Pletinckx <pieplay@gmail.com>
2017-08-03 15:42:47 +0200

    start off project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PartsComponent } from './parts/parts.component';
+import { PreviewComponent } from './preview/preview.component';
+import { PresetComponent } from './preset/preset.component';
+import { ModelComponent } from './model/model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartsComponent,
+    PreviewComponent,
+    PresetComponent,
+    ModelComponent
   ],
   imports: [
     BrowserModule
